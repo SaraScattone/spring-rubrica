114a3676e6e987a2d453d23f0484e7f92c3efd9b
SaraScattone <sara.scattone@marconirovereto.it>
2024-04-22 10:20:22 +0200

    scrittura codice contactservice e aggiornamento codice controller

--- src/main/java/it/marconi/springrubrica/controllers/ContactController.java
+++ src/main/java/it/marconi/springrubrica/controllers/ContactController.java
@@ -1,5 +1,8 @@
 package it.marconi.springrubrica.controllers;
 
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -7,11 +10,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import it.marconi.springrubrica.domains.Contact;
 import it.marconi.springrubrica.domains.ContactForm;
+import it.marconi.springrubrica.services.ContactService;
 
 @Controller
 @RequestMapping("/contacts")
 public class ContactController {
+
+    @Autowired
+    private ContactService contactService;
    
     @GetMapping("/new")
     public ModelAndView newContactForm() {
@@ -21,10 +30,19 @@ public class ContactController {
     @PostMapping("/new")
     public ModelAndView handleNewContact(@ModelAttribute ContactForm contactForm) {
 
+        Contact contact = contactService.save(contactForm);
+
         return new ModelAndView("contact-detail").addObject("contact", contactForm);
 
         //necessita pattern PRG
     }
+
+    @GetMapping(params = "id")
+    public ModelAndView showContact(@RequestParam("id") UUID contactId) {
+
+        
+    }
+    
 
     /*
     @GetMapping(params = "id")
--- src/main/java/it/marconi/springrubrica/services/ContactService.java
+++ src/main/java/it/marconi/springrubrica/services/ContactService.java
@@ -1,7 +1,12 @@
 package it.marconi.springrubrica.services;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import it.marconi.springrubrica.domains.Contact;
+import it.marconi.springrubrica.domains.ContactForm;
 import it.marconi.springrubrica.repositories.ContactRepository;
 
 @Service
@@ -10,5 +15,26 @@ public class ContactService {
     @Autowired //serve a forzare la dependacy injection
     private ContactRepository contactRepo;
 
-    
+    public Contact save(ContactForm contactForm) {
+
+        Contact c = mapContact(contactForm);
+
+        return contactRepo.save(c);
+    }
+
+    private Contact mapContact(ContactForm contactForm) {
+
+        Contact c = new Contact();
+
+        c.setName(contactForm.getName());
+        c.setSurname(contactForm.getSurname());
+        c.setPhone(contactForm.getPhone());
+        c.setEmail(contactForm.getEmail());
+
+        return c;
+    }
+
+    public Contact get(UUID id) {
+        return contactRepo.findById(id);
+    }
 }
